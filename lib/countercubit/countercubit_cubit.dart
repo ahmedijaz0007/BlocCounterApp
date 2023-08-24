b8869342e13a2bd5064b0f53d1752c747b704f1d
ahmed ijaz <ahmedijaz0007@gmail.com>
2023-08-24 01:12:19 -0700

    uploading bloc counterApp

---
+++ lib/countercubit/countercubit_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'countercubit_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counterValue: 0));
+  
+  increment() => emit(CounterState(counterValue: state.counterValue+1,isIncremented: true));
+  decrement() => emit(CounterState(counterValue: state.counterValue-1,isIncremented: false));
+}
---
+++ lib/countercubit/countercubit_state.dart
@@ -0,0 +1,7 @@
+part of 'countercubit_cubit.dart';
+
+class CounterState{
+  CounterState({required this.counterValue, this.isIncremented});
+  int counterValue;
+  bool? isIncremented;
+}
